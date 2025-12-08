282ac9a04638bdbd6e9f0135412685d92c40e434
Han Xu <hxu25085@gmail.com>
2025-12-08 03:53:12 -0600

    Upload the final version and comments of code

--- Jurassic_Park_Code.ino
+++ Jurassic_Park_Code.ino
@@ -6,16 +6,17 @@ Team Members: Claire Huang, Siddharth Ramalingam, Han Xu
 
 Descritpion:
 This program controls 5 interactive scenes in the enclosure, each triggered by its own switch:
-1. The switch triggers haptic motor vibration.
-2. The goat head rotates to reveal its death.
-3. The dinosaur flips up as a jump scare.
-4. The car flips upside down.
-5. The car lands in a tree and plays the ending melody.
+1. Haptic motor vibrates to simulate the T-Rex footsteps.
+2. Goat body rotates to reveal its death.
+3. Dinosaur flips up as a jump scare.
+4. Car flips upside down.
+5. Characters escape and the ending victory melody plays.
 */
 
 #include <Servo.h>
 #include "pitches.h";
 
+// Scene Class - Detect switch state changes to execute corresponding scene actions
 class Scene{
   private:
     int switchPin;
@@ -29,31 +30,38 @@ class Scene{
       previousState = 0;
     }
   
+  // Attach switch to specified pin
   void attachPin(int pin){
     switchPin = pin;
     pinMode(switchPin, INPUT);
   }
 
-  void update(int sceneNum, void (*onPress)(), void (*onRelease)() = nullptr){
+  // Monitor switch state and trigger action on activation
+  void update(int sceneNum, void (*action)()){
+    // sceneNum: Scene number (1-5) for which scene is being updated
+    // action: Function to execute when switch is activated
     currentState = digitalRead(switchPin);
-    if (currentState != previousState) {
+    
+    if (currentState != previousState) {''
       if (currentState == HIGH) {
+        // Switch activated - print message and perform the specific action
         Serial.print("switch ");
         Serial.print(sceneNum);
         Serial.println(" activated");
-        onPress();
+        action();
       } else {
+        // Switch released - print message
         Serial.print("switch ");
         Serial.print(sceneNum);
         Serial.println(" released");
-        onRelease();
       }
     }
+    
     previousState = currentState;
   }
 };
 
-// Create Scene objects
+// Initialize Scenes, Servos, and Pins
 Scene scene1, scene2, scene3, scene4, scene5;
 
 Servo goatServo;
@@ -63,6 +71,7 @@ Servo carServo;
 const int motorPin = 5;
 const int piezoPin = 8;
 
+// Define notes and durations for the Ending Melody
 int melody[] = {
   NOTE_C4, NOTE_D4, NOTE_E4, NOTE_G4, NOTE_E4, NOTE_G4
 };
@@ -70,6 +79,7 @@ int noteDurations[] = {
   8, 8, 8, 4, 8, 2
 };
 
+// Initalize serial communication and attach pins 
 void setup() {
   Serial.begin(9600);
   Serial.println("Start Testing");
@@ -87,51 +97,60 @@ void setup() {
   pinMode(motorPin, OUTPUT);
 }
 
+// Update each scene with its corresponding action
 void loop() {
   scene1.update(1, vibrateMotor);
-  scene2.update(2, rotateGoat, resetGoat);
+  scene2.update(2, rotateGoat);
   scene3.update(3, flipDino);
-  scene4.update(4, flipCar, resetCar);
+  scene4.update(4, flipCar);
   scene5.update(5, playEndingMelody);
 }
 
 // Scene 1: Haptic Motor Vibration
 void vibrateMotor() {
+  // Simulate T-Rex footsteps with 3 vibration pulses
   digitalWrite(motorPin, HIGH);
   delay(300);
   digitalWrite(motorPin, LOW);
   delay(1200);
+  
   digitalWrite(motorPin, HIGH);
   delay(300);
   digitalWrite(motorPin, LOW);
   delay(1200);
+
   digitalWrite(motorPin, HIGH);
   delay(300);
   digitalWrite(motorPin, LOW);
   delay(1800);
 }
 
-// Scene 2: Rotate Goat Head
+// Scene 2: Goat Rotation
 void rotateGoat(){
+  // Rotate goat
   goatServo.write(180);
   Serial.println("Goat rotated");
-}
 
-// Scene 2: Reset
-void resetGoat(){
+  // Hold for 5 seconds
   delay(5000);
+
+  // Rest goat position
   goatServo.write(0);
   Serial.println("Goat reset");
 }
 
-// Scene 3: Dinosaur Movement
-// A 360 degree Servo is used here
+// Scene 3: Dinosaur Jump Scare
 void flipDino(){
+  // Flip dinosaur up
   dinoServo.write(180);
   delay(2750);
+  Serial.println("Dinosaur Up");
+
+  // Hold for 5 seconds
   dinoServo.write(90);
   delay(5000);
-  Serial.print("Dinosaur Up");
+
+  // Flip dinosaur down
   dinoServo.write(0);
   delay(2750);
   dinoServo.write(90);
@@ -140,13 +159,14 @@ void flipDino(){
 
 // Scene 4: Car Flip
 void flipCar(){
+  // Flip car upside down
   carServo.write(-180);
   Serial.println("Car flipped");
-}
 
-// Scene 4: Reset 
-void resetCar(){
+  // Hold for 5 seconds
   delay(5000);
+
+  // Reset car position
   carServo.write(180);
   Serial.println("Car reset");
 }
@@ -155,10 +175,10 @@ void resetCar(){
 void playEndingMelody() {
   Serial.println("Melody ON");
   for (int thisNote = 0; thisNote < 6; thisNote++) {
-    Serial.println(melody[thisNote]);
     // Play each note for its duration
     int noteDuration = 1000 / noteDurations[thisNote];
     tone(piezoPin, melody[thisNote], noteDuration);
+
     // Pause between notes
     int pauseBetweenNotes = noteDuration * 1.30;
     delay(pauseBetweenNotes);
